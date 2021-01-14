d8a354858c41cbdb1e1c43e1def4d0cdd0294c97
Mike Hankinson <70593340+MikeHankinson@users.noreply.github.com>
2021-01-14 15:19:32 -0600

    Add Mapping Multiple Points - Section 13.4.2

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -2,8 +2,8 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
-
+// let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 // TILE LAYER -- background of the map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -17,23 +17,51 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 // Then add the 'graymap' tile layer to the map.
 streets.addTo(map);
 
-//  Add a MARKER to the map for Los Angeles, California. 4.3.1
-let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// -----------------------------------------------------------------------
+// Map a Single Point - 13.4.1
+// -----------------------------------------------------------------------
 
-//  Add a CIRCLE to the map for Los Angeles, California. 4.3.1
-// OPTION 1
-L.circle([34.0522, -118.2437], {
-    radius: 100,
-    color: "red"
-    // fill opacity:
- }).addTo(map);
+// //  Add a MARKER to the map for Los Angeles, California. 
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
-//  Add a CIRCLE to the map for Los Angeles, California. 4.3.1
-// OPTION 2
-L.circleMarker([34.0522, -118.2437], {
-    radius: 300,
-    color: "black",
-    // fill color: '#ffffa1'
-}).addTo(map);
+// //  Add a CIRCLE to the map for Los Angeles, California. 
+// // OPTION 1
+// L.circle([34.0522, -118.2437], {
+//     radius: 100,
+//     color: "red"
+//     // fill opacity:
+//  }).addTo(map);
+
+// //  Add a CIRCLE to the map for Los Angeles, California. 
+// // OPTION 2
+// L.circleMarker([34.0522, -118.2437], {
+//     radius: 300,
+//     color: "black",
+//     // fill color: '#ffffa1'
+// }).addTo(map);
+
+// -----------------------------------------------------------------------
+// Map Multiple Points - 13.4.2
+// -----------------------------------------------------------------------
+// Replace MARKER variable with CITYDATA variable.    
+// Get data from cities.js
+let cityData = cities;
+
+// // Loop through the cities array and create one MARKER for each city.
+// cityData.forEach(function(city) {
+//     console.log(city)
+//     L.marker(city.location).addTo(map)
+//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>");
+// });
 
 
+// Loop through the cities array and create CIRCLE with radius
+// proportional to city's population
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/100000
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+});
